f247fdd772136f5292c4f0e76dba04425d2645fd
Gabriel Luiz <gabriel.luiz77888@gmail.com>
2023-12-11 22:29:21 -0300

    Configurando CORS

--- api/src/main/kotlin/com/example/sequence_alignment/controller/AppController.kt
+++ api/src/main/kotlin/com/example/sequence_alignment/controller/AppController.kt
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
+
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = ["*"])
 class AppController(
     private val wordAnalyser: WordAnalyser
 ) {
